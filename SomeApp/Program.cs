06bc9151abbc6293bc7e69ee199dc1cd0533b327
Abhinav Kant <abhinav.kant@gmail.com>
2024-02-02 23:36:10 +0530

    feat: more changes

--- SomeApp/Program.cs
+++ SomeApp/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+namespace DotnetFormatExample
+{
+    public class Calculator
+    {
+        public int AddNumbers(int one, int two)
+        {
+            int finalNumber = 0;
+
+            finalNumber = one + two;
+
+            return finalNumber;
+        }
+    }
+}
