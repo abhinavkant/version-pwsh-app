900a84094794ae788a35e38510c377af9375e8c9
Abhinav Kant <abhinav.kant@gmail.com>
2024-02-03 08:09:54 +0530

    feat: more changes

---
+++ SomeApp/Calculator.cs
@@ -0,0 +1,12 @@
+namespace DotnetFormatExample
+{
+    public class Calculator
+    {
+        public int AddNumbers(int one, int two)
+        {
+            var finalNumber = 0;
+            finalNumber = one + two;
+            return finalNumber;
+        }
+    }
+}
--- SomeApp/Program.cs
+++ SomeApp/Program.cs
@@ -1,20 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-namespace DotnetFormatExample
-{
-    public class Calculator
-    {
-        public int AddNumbers(int one, int two)
-        {
-            string name = "Abhuinav";
-            Console.WriteLine(name);
-
-            int finalNumber = 0;
-
-            finalNumber = one + two;
-
-            return finalNumber;
-        }
-    }
-}
